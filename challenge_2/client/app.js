bed055c920fa802c627437a95bf48dd9f408d650
dmai261 <dxmai1314@gmail.com>
2018-08-28 19:33:32 -0700

    finished bare requirements

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,27 @@
+$('#submit_btn').on('click', function(e) {
+    e.preventDefault();
+    let textData = $('textarea').val();
+    postHandler(textData);
+});
 
-
+let postHandler = (postData) => {
+    $.ajax({
+        url: 'http://localhost:3000/data',
+        type: 'POST',
+        data: postData,
+        contentType: 'application/json',
+        success: function(data) {
+            data.forEach((row)=> {
+                let str = '';
+                row.forEach(function(element){
+                    str+= element + ' | ';
+                })
+                let t = ('<div>' + str + '</div>')
+                $('#csv').append(t);
+            });
+        },
+        error: function(err) {
+            console.log(err)
+        }
+    })
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,45 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+// let store = [];
 
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+app.get('/data', (req, res) =>
+  res.send(store)
+)
+
+app.post('/data', (req, res) => {
+  let newData = deconstructor((req.body));
+  let headers = [];
+  for (var key in req.body) {
+      if (key !== 'children') {
+          headers.push(key);
+      }
+  }
+  newData.unshift(headers);
+  res.send(newData);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+let deconstructor = function(data) {
+  let results = arguments[1] || [];
+  let arr = [];
+  for (var key in data) {
+    if (key !== 'children') {
+      arr.push(data[key]);
+    }
+  }
+  results.push(arr);
+  if (data.children.length === 0 || data.children === undefined) {
+    return;
+  } else {
+    data.children.forEach((child)=>{
+      deconstructor(child, results)
+    })
+  }
+  return results
+}
